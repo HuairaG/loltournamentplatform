3fd4368044e15467115eae6ce7f76b80553a856b
HuairaG <ldarkllarg@gmail.com>
2018-09-05 18:29:14 -0300

    setup mysql database, user creation and login

---
+++ leagueoffreljord/userforms.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
+class UserForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'email',
+        ]
+        labels = {
+            'username': 'Nombre de usuario',
+            'email': 'Correo',
+        }
--- leagueoffreljord/views.py
+++ leagueoffreljord/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from . import userviews
 # Create your views here.
 
 def home(request):
