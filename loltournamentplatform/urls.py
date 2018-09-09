1185d4e5e655be588b681b6e9c73929aeb844d96
HuairaG <ldarkllarg@gmail.com>
2018-09-09 18:06:15 -0300

    logout implementado

--- loltournamentplatform/urls.py
+++ loltournamentplatform/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('leagueoffreljord.urls', namespace='api')),
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout')
 ]
